df612d53fb639056b18a67926dacb725de67019e
viparthasarathy <viparthasarathy@gmail.com>
2016-08-12 10:10:48 -0700

    Add restful API routes

--- server.js
+++ server.js
@@ -13,6 +13,45 @@ app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
+const Todo = mongoose.model('Todo', {
+  text : String
+});
+
+app.get('/apitodos', (req, res) => {
+  Todo.find((err, todos) => {
+    if (err) { res.send(err); }
+    res.json(todos);
+
+  });
+});
+
+app.post('/api/todos', (req, res) => {
+  Todo.create({
+    text : req.body.text,
+    done : false
+  }, function(err, todo) {
+    if (err) { res.send(err); }
+
+    Todo.find(function(err, todos) {
+      if (err) { res.send(err); }
+      res.json(todos);
+    });
+  });
+});
+
+app.delete('/api/toos/:todo_id', (req, res) => {
+  Todo.remove({
+    _id: req.params.todo_id
+  }, (err, todo) => {
+    if (err) { res.send(err); }
+
+    Todo.find((err, todos) => {
+      if (err) { res.send(err); }
+      res.json(todos);
+    });
+  });
+});
+
 app.listen(8080, function() {
   console.log("Listening on port 8080");
 });
